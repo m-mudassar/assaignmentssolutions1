2b7d48d9e66a33cef3fa8b5a5dc00f7aa7fb6c44
codebiters <Malik.492875>
2021-12-09 12:31:06 +0500

    All question solved except question 6

--- src/softwarehouse/vu/Solution10.java
+++ src/softwarehouse/vu/Solution10.java
@@ -1,12 +1,9 @@
 package softwarehouse.vu;
 
-import com.sun.source.tree.BinaryTree;
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution10 {
     // Read numeric values from text file (at-least 500 values),
@@ -14,23 +11,102 @@ public class Solution10 {
     // and then traverse it using in-order traversal
 
     public static void main(String[] args) {
-
+        BinaryTree tree = new BinaryTree();
         File file = new File("src/softwarehouse/vu/numbers.txt");
 
         try {
             FileReader fr = new FileReader(file);
             int content;
             while ((content = fr.read()) != -1) {
-                if ((char)content >= '0' && (char)content <= '9'){
-                    // add to binary tree
-                System.out.print((char)content + " ");
+                char ch =(char) content;
+                if (ch >= '0' && ch <= '9') {
+                    tree.insert(Integer.parseInt(String.valueOf(ch)));
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        tree.inOrder(tree.root);
     }
 
+    protected static class BinaryTree {
+        public BinaryNode root;
+        public BinaryTree(){
+            this.root = null;
+        }
 
+
+        public void insert(int value) {
+            BinaryNode node = new BinaryNode();
+            node.setValue(value);
+            if (root == null) {
+                root = node;
+                return;
+            }
+            Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                BinaryNode presentNode = queue.remove();
+                if (presentNode.getLeft() == null) {
+                    presentNode.setLeft(node);
+                    break;
+                }else if (presentNode.getRight() == null) {
+                    presentNode.setRight(node);
+                    break;
+                } else {
+                    queue.add(presentNode.getLeft());
+                    queue.add(presentNode.getRight());
+                }
+            }
+        }
+
+        public void inOrder(BinaryNode node) {
+            if (node == null) {
+                return;
+            }
+            inOrder(node.getLeft());
+            System.out.print(node.getValue() + " ");
+            inOrder(node.getRight());
+        }
+
+    }
+
+    protected static class BinaryNode {
+        private int value;
+        private BinaryNode left;
+        private BinaryNode right;
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public BinaryNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(BinaryNode left) {
+            this.left = left;
+        }
+
+        public BinaryNode getRight() {
+            return right;
+        }
+
+        public void setRight(BinaryNode right) {
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return value + "";
+        }
+
+    }
 }
+
+
